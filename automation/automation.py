22f7dc9154b619ab5016207b2163ed92afecba57
abdu-zeyad <abd.d@live.com>
2021-08-18 22:14:44 +0300

    finsih half the lap

--- automation/automation.py
+++ automation/automation.py
@@ -1,7 +1,17 @@
 import shutil
 import re
+from typing import Counter
 
-content = ''
-with open('potential-contacts.txt', 'w+') as file:
-    file.write(content)
-shutil.copy('potential-contacts.txt', 'potential-contacts.txt.bak')
+# numbers = re.findall('[0-9]+', str)
+# with open('potential-contacts.txt', 'w+') as file:
+#     # file.write(numbers)
+#     x = file.read()s
+# print(x)
+# shutil.copy('potential-contacts.txt', 'potential-contacts.txt.bak')
+
+
+f = open("assets1/potential-contacts.txt", "r").read()
+numbers = re.findall(r'(\d\d\d-\d\d\d-\d\d\d\d)', f)
+print(numbers)
+match = re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', f)
+print(match)
